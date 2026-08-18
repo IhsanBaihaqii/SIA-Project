4c4d7efed4b8eca826678a57cdf45742b0aadfba
hairulmahesa <hairulmahesa@gmai.com>
2026-08-18 10:56:05 +0700

    Database SIA

---
+++ sia_project.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2026 at 05:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sia_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_products`
+--
+
+CREATE TABLE `tbl_products` (
+  `id_product` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaction`
+--
+
+CREATE TABLE `tbl_transaction` (
+  `id_transaction` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaction_details`
+--
+
+CREATE TABLE `tbl_transaction_details` (
+  `id_transaction_detail` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `id_transaction` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- Indexes for table `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  ADD PRIMARY KEY (`id_transaction`);
+
+--
+-- Indexes for table `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  ADD PRIMARY KEY (`id_transaction_detail`),
+  ADD KEY `id_product` (`id_product`),
+  ADD KEY `id_transaction` (`id_transaction`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  MODIFY `id_transaction` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  MODIFY `id_transaction_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  ADD CONSTRAINT `tbl_transaction_details_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `tbl_products` (`id_product`),
+  ADD CONSTRAINT `tbl_transaction_details_ibfk_2` FOREIGN KEY (`id_transaction`) REFERENCES `tbl_transaction` (`id_transaction`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
