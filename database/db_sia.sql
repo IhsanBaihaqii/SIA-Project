c42eec5c02faf9c88f95714f7df4c2c3d793d798
ihsanbaihaqii <baihaqiihsan31@gmail.com>
2026-08-20 20:40:33 +0700

    edit: database

--- database/db_sia.sql
+++ database/db_sia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2026 at 05:41 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 20 Agu 2026 pada 15.39
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_products`
+-- Struktur dari tabel `tbl_products`
 --
 
 CREATE TABLE `tbl_products` (
@@ -37,7 +37,7 @@ CREATE TABLE `tbl_products` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_transaction`
+-- Struktur dari tabel `tbl_transaction`
 --
 
 CREATE TABLE `tbl_transaction` (
@@ -49,7 +49,7 @@ CREATE TABLE `tbl_transaction` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_transaction_details`
+-- Struktur dari tabel `tbl_transaction_details`
 --
 
 CREATE TABLE `tbl_transaction_details` (
@@ -61,70 +61,10 @@ CREATE TABLE `tbl_transaction_details` (
   `subtotal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_products`
---
-ALTER TABLE `tbl_products`
-  ADD PRIMARY KEY (`id_product`);
-
---
--- Indexes for table `tbl_transaction`
---
-ALTER TABLE `tbl_transaction`
-  ADD PRIMARY KEY (`id_transaction`);
-
---
--- Indexes for table `tbl_transaction_details`
---
-ALTER TABLE `tbl_transaction_details`
-  ADD PRIMARY KEY (`id_transaction_detail`),
-  ADD KEY `id_product` (`id_product`),
-  ADD KEY `id_transaction` (`id_transaction`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_products`
---
-ALTER TABLE `tbl_products`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_transaction`
---
-ALTER TABLE `tbl_transaction`
-  MODIFY `id_transaction` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `tbl_transaction_details`
---
-ALTER TABLE `tbl_transaction_details`
-  MODIFY `id_transaction_detail` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tbl_transaction_details`
---
-ALTER TABLE `tbl_transaction_details`
-  ADD CONSTRAINT `tbl_transaction_details_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `tbl_products` (`id_product`),
-  ADD CONSTRAINT `tbl_transaction_details_ibfk_2` FOREIGN KEY (`id_transaction`) REFERENCES `tbl_transaction` (`id_transaction`);
-COMMIT;
-
---
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_user`
+-- Struktur dari tabel `tbl_user`
 --
 
 CREATE TABLE `tbl_user` (
@@ -139,21 +79,70 @@ CREATE TABLE `tbl_user` (
 --
 
 --
--- Indexes for table `tbl_user`
+-- Indeks untuk tabel `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  ADD PRIMARY KEY (`id_product`);
+
+--
+-- Indeks untuk tabel `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  ADD PRIMARY KEY (`id_transaction`);
+
+--
+-- Indeks untuk tabel `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  ADD PRIMARY KEY (`id_transaction_detail`),
+  ADD KEY `id_product` (`id_product`),
+  ADD KEY `id_transaction` (`id_transaction`);
+
+--
+-- Indeks untuk tabel `tbl_user`
 --
 ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tbl_user`
+-- AUTO_INCREMENT untuk tabel `tbl_products`
+--
+ALTER TABLE `tbl_products`
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_transaction`
+--
+ALTER TABLE `tbl_transaction`
+  MODIFY `id_transaction` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  MODIFY `id_transaction_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user`
 --
 ALTER TABLE `tbl_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_transaction_details`
+--
+ALTER TABLE `tbl_transaction_details`
+  ADD CONSTRAINT `tbl_transaction_details_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `tbl_products` (`id_product`),
+  ADD CONSTRAINT `tbl_transaction_details_ibfk_2` FOREIGN KEY (`id_transaction`) REFERENCES `tbl_transaction` (`id_transaction`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
